e7c4a124fe5c8d5d8e48afb3fd445297ed4274fc
JovitaBu <53538545+JovitaBu@users.noreply.github.com>
2020-10-15 12:33:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <time.h>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
@@ -12,19 +15,34 @@ using std::vector;
 
 struct Data {
     string name, surname;
-    float exam, homework[20], finalGrade;       /*namų darbams imamas masyvas su daugiausiai 20 elementų,
-                                                  nes viename semestre yra vidutiniškai 15-16 savaičių*/
+    float exam, homework[20], finalGrade, finalGradeMean, finalGradeMedian;
 };
 
+
+bool compareNames(Data firstStudent, Data secondStudent){
+    
+    if (firstStudent.name != secondStudent.name){
+        return firstStudent.name < secondStudent.name;
+    }
+
+    else{
+        return firstStudent.surname < secondStudent.surname;
+    }
+}
+
 float countMean(float Array[], int size){
+
     float sum = 0;
+
     for (int i = 0; i < size; i++){
         sum += Array[i];
     }
+
     return sum / (size * 1.0);
 }
 
 float findMedian(float Array[], int size){
+
     for (int i = 0; i < size - 1; i++){
         for (int j = i + 1; j < size; j++){
             if (Array[i] > Array[j]){
@@ -32,92 +50,141 @@ float findMedian(float Array[], int size){
             }
         }
     }
+
     if (size % 2 == 0){
         return (Array[size / 2] + Array[size / 2 - 1]) * 1.0 / 2;
     }
+    
     else {
         return Array[size / 2];
     }
 }
 
-int main() {
-    int countBy;
-    int studentCount;
-    int doRandom;
-    cout << "Pasirinkite:\n(1) skaiciuoti pagal namu darbu vidurki;\n(2) skaiciuoti pagal namu darbu mediana: ";
-    cin >> countBy;
-    cout << "Studentu skaicius: ";
-    cin >> studentCount;
-    cout<< "Ar norite egzamino ir namu darbo pazymius generuoti atsitiktinai? (1) Taip; (2) ne: ";
-    cin >> doRandom;
-
-    vector<Data> student;
-    student.reserve(studentCount);
-    Data temporary;
-    int sum;
-    int countHomework;
+float countFinal(Data student, int countBy, int countHomework){
     
-    if (doRandom == 2){
-        for (int i = 0; i < studentCount; i++){
-            sum = 0;
-            cout << "Iveskite studento varda, pavarde, egzamino ivertinima ir namu darbus (uzbaigimui iveskite 0): ";
-            cin >> temporary.name >> temporary.surname >> temporary.exam;
-            countHomework = 0;
-            cin >> temporary.homework[countHomework];
-            while (temporary.homework[countHomework] != 0){
-                sum += temporary.homework[countHomework];
-                countHomework++;
-                cin >> temporary.homework[countHomework];
-            }
-            if (countBy == 1){
-                temporary.finalGrade = countMean(temporary.homework, countHomework) * 0.4 + temporary.exam * 0.6;
-            }
-            if (countBy == 2){
-                temporary.finalGrade = findMedian(temporary.homework, countHomework) * 0.4 + temporary.exam * 0.6;
-            }
-            student.push_back(temporary);
-        }
-    }
-
-    if (doRandom == 1){
-        for (int i = 0; i < studentCount; i++){
-        cout << "Iveskite studento varda ir pavarde: ";
-        cin >> temporary.name >> temporary.surname;
-        srand(time(0));
-        temporary.exam = rand() % 10 + 1;
-        countHomework = rand() % 21;
-        if (countHomework == 0){
-            student[i].homework[0] = 0;
-            countHomework = 1;
-        }
-        for (int j = 0; j < countHomework; j++){
-            temporary.homework[j] = rand() % 10 + 1;
-        }
-        if (countBy == 1){
-            temporary.finalGrade = countMean(temporary.homework, countHomework) * 0.4 + temporary.exam * 0.6;
-        }
-        if (countBy == 2){
-            temporary.finalGrade = findMedian(temporary.homework, countHomework) * 0.4 + temporary.exam * 0.6;
-        }
-        student.push_back(temporary);
-        }
-    }
-
-
-    cout << "\n\n";
     if (countBy == 1){
-    cout << "Vardas\tPavarde\t\t\tGalutinis (vid.)\n";
+        return countMean(student.homework, countHomework) * 0.4 + student.exam * 0.6;
     }
-    if (countBy == 2){
-    cout << "Vardas\tPavarde\t\t\tGalutinis (med.)\n";
-    }
-    cout << "------------------------------------------------------\n";
-    cout.precision(3);
-    for (int i = 0; i < studentCount; i++){
-        cout << student[i].name << "\t\t" << student[i].surname << "\t\t" << student[i].finalGrade << "\n";
 
+    if (countBy == 2){
+        return findMedian(student.homework, countHomework) * 0.4 + student.exam * 0.6;
+    }
+
+}
+
+int main() {
+
+    int readData;
+    int studentCount;
+
+    cout << "Pasirinkite:\n(1) duomenis ivesti;\n(2) duomenis nuskaityti is kursiokai.txt failo: ";
+    cin >> readData;
+
+    if (readData == 1){
+        int countBy;
+        int doRandom;
+        
+        cout << "\nPasirinkite:\n(1) skaiciuoti pagal namu darbu vidurki;\n(2) skaiciuoti pagal namu darbu mediana: ";
+        cin >> countBy;
+        cout << "\nStudentu skaicius: ";
+        cin >> studentCount;
+        cout << "\nAr norite egzamino ir namu darbo pazymius generuoti atsitiktinai?\n(1) Taip;\n(2) ne: ";
+        cin >> doRandom;
+        cout << "\n";
+
+        vector<Data> student;
+        student.reserve(studentCount);
+        Data temporary;
+        int countHomework;
+        
+        if (doRandom == 2){
+            for (int i = 0; i < studentCount; i++){
+                cout << "Iveskite studento varda, pavarde, egzamino ivertinima ir namu darbus (uzbaigimui iveskite 0): ";
+                cin >> temporary.name >> temporary.surname >> temporary.exam;
+                countHomework = 0;
+                cin >> temporary.homework[countHomework];
+                while (temporary.homework[countHomework] != 0){
+                    countHomework++;
+                    cin >> temporary.homework[countHomework];
+                }
+                temporary.finalGrade = countFinal(temporary, countBy, countHomework);
+                student.push_back(temporary);
+            }
+        }
+
+        if (doRandom == 1){
+            for (int i = 0; i < studentCount; i++){
+            cout << "Iveskite studento varda ir pavarde: ";
+            cin >> temporary.name >> temporary.surname;
+            srand(time(0));
+            temporary.exam = rand() % 10 + 1;
+            countHomework = rand() % 21;
+            if (countHomework == 0){
+                student[i].homework[0] = 0;
+                countHomework = 1;
+            }
+            for (int j = 0; j < countHomework; j++){
+                temporary.homework[j] = rand() % 10 + 1;
+            }
+            temporary.finalGrade = countFinal(temporary, countBy, countHomework);
+            student.push_back(temporary);
+            }
+        }
+
+        cout << "\n\n";
+        
+        if (countBy == 1){
+            cout << std::left << std::setw(20) << "Vardas" << std::setw(20) << "Pavarde" << std::setw(20) << "Galutinis (vid.)";
+        }
+
+        if (countBy == 2){
+            cout << std::left << std::setw(20) << "Vardas" << std::setw(20) << "Pavarde" << std::setw(20) << "Galutinis (med.)";
+        }
+
+        cout << "\n------------------------------------------------------\n";
+        cout.precision(3);
+        for (int i = 0; i < studentCount; i++){
+            cout << std::left << std::setw(20) << student[i].name << std::setw(20) << student[i].surname << std::setw(20) << student[i].finalGrade << "\n";
+        }
+
+    }
+
+
+
+    if (readData == 2){
+
+        vector<Data> student;
+        Data temporary;
+
+        std::ifstream dataFile("kursiokai.txt");
+        string firstLine;
+        getline(dataFile, firstLine);
+        studentCount = 0;
+
+        while (!dataFile.eof()){
+            dataFile >> temporary.name >> temporary.surname;
+            for (int i = 0; i < 5; i++){
+                dataFile >> temporary.homework[i];
+            }
+            dataFile >> temporary.exam;
+            temporary.finalGradeMean = countFinal(temporary, 1, 5);
+            temporary.finalGradeMedian = countFinal(temporary, 2, 5);
+            student.push_back(temporary);
+            studentCount++;
+        }
+
+        dataFile.close();
+
+        sort(student.begin(), student.end(), compareNames);
+
+        cout << "\n\n";
+        cout << std::left << std::setw(20) << "Vardas" << std::setw(20) << "Pavarde" << std::setw(20) << "Galutinis (vid.)" << std::setw(20) << "Galutinis (med.)";
+        cout << "\n--------------------------------------------------------------------------------\n";
+        cout.precision(3);
+        for (int i = 0; i < studentCount; i++){
+            cout << std::left << std::setw(20) << student[i].name << std::setw(20) << student[i].surname << std::setw(20) << student[i].finalGradeMean << std::setw(20) << student[i].finalGradeMedian << "\n";
+        }
     }
 
     return 0;
-
 }
