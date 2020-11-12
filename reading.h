501690e0d0e25475335bb6dc051cdfd19c026814
JovitaBu <53538545+JovitaBu@users.noreply.github.com>
2020-11-12 12:55:23 +0200

    Funkcija failo nuskaitymui į sąrašą

--- reading.h
+++ reading.h
@@ -5,5 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <list>
 
 void readFile(std::vector<Data> &student, int studentCount, int homeworkCount, std::string fileName);
+void readFile(std::list<Data> &student, int studentCount, int homeworkCount, std::string fileName);
