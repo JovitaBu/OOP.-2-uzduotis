b2f01ad3f0bab46813905a719653e0cf338734ab
JovitaBu <53538545+JovitaBu@users.noreply.github.com>
2020-11-12 12:50:18 +0200

    Funkcija failo sukūrimui iš sąrašo

--- createFiles.h
+++ createFiles.h
@@ -7,6 +7,8 @@
 #include <iomanip>
 #include <chrono>
 #include <random>
+#include <list>
 
 void generateFile(std::string fileName, int studentCount, int homeworkCount);
 void writeFile(std::string fileName, std::vector<Data> student, int homeworkCount, std::string studentType);
+void writeFile(std::string fileName, std::list<Data> student, int homeworkCount, std::string studentType);
