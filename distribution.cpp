e832867d2f9d64e48d960b84e5abcdc1fc8245b6
JovitaBu <53538545+JovitaBu@users.noreply.github.com>
2020-11-29 19:35:57 +0200

    Naujas skirstymas

--- distribution.cpp
+++ distribution.cpp
@@ -1,49 +1,40 @@
 #include "distribution.h"
 
-void distribute(std::vector<Data> student, std::vector<Data> &good, std::vector<Data> &bad){
-
-    //vektoriams paskiriama pusė viso studentų vektoriaus dydžio
-    good.reserve(student.size()/2);
-    bad.reserve(student.size()/2);
+void distribute(std::vector<Data> &student, std::vector<Data> &bad){
+    
+    int n = student.size();
+    bad.reserve(n);
 
     //pradedamas skaičiuoti laikas
     auto start = std::chrono::high_resolution_clock::now();
 
-    //skirstomi studentai
-    for (int i = 0; i < student.size(); i++){
-        if (student[i].finalGradeMean < 5.00){
-            bad.push_back(student[i]);
-        }
-        else{
-            good.push_back(student[i]);
-        }
-    }
+    //studentai, kurių vidurkis <5, nukopijuojami į atskirą vektorių ir tada ištrinami
+    std::copy_if(student.begin(), student.end(), std::back_inserter(bad), [](Data s){return s.finalGradeMean < 5;});
+    student.erase(std::remove_if(student.begin(), student.end(), [](Data s){return s.finalGradeMean < 5;}), student.end());
 
     //stabdomas laiko skaičiavimas
     auto end = std::chrono::high_resolution_clock::now();
 
     //išvedamas laiko skaičiavimo rezultatas
-    std::cout << student.size() << " studentu suskirstyti pagal vidurki per " << std::chrono::duration<double>(end - start).count() << " s." << std::endl;
+    std::cout << n << " studentu suskirstyti per " << std::chrono::duration<double>(end - start).count() << " s." << std::endl;
+
 }
 
-void distribute(std::list<Data> student, std::list<Data> &good, std::list<Data> &bad){
+void distribute(std::list<Data> &student, std::list<Data> &bad){
+        
+    int n = student.size();
 
     //pradedamas skaičiuoti laikas
     auto start = std::chrono::high_resolution_clock::now();
 
-    //skirstomi studentai
-    for (Data s : student){
-        if (s.finalGradeMean < 5.00){
-            bad.push_back(s);
-        }
-        else{
-            good.push_back(s);
-        }
-    }
+    //studentai, kurių vidurkis <5, nukopijuojami į atskirą sąrašą ir tada ištrinami
+    std::copy_if(student.begin(), student.end(), std::back_inserter(bad), [](Data s){return s.finalGradeMean < 5;});
+    student.erase(std::remove_if(student.begin(), student.end(), [](Data s){return s.finalGradeMean < 5;}), student.end());
 
     //stabdomas laiko skaičiavimas
     auto end = std::chrono::high_resolution_clock::now();
 
     //išvedamas laiko skaičiavimo rezultatas
-    std::cout << student.size() << " studentu suskirstyti pagal vidurki per " << std::chrono::duration<double>(end - start).count() << " s." << std::endl;
+    std::cout << n << " studentu suskirstyti per " << std::chrono::duration<double>(end - start).count() << " s." << std::endl;
+
 }
